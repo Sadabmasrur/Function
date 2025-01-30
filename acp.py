47ef2ed412b312fe2d12e27ccab3cf8506d4a442
Sadab Masrur <sadabmasrur08@gmail.com>
2025-01-30 10:30:56 +0600

    acp

---
+++ acp.py
@@ -0,0 +1,10 @@
+import math
+
+radius=float(input("Enter the radius of the circle: "))
+
+circumference=2*math.pi*radius
+print(f"Circumference of the circle: {circumference}")
+
+area=math.pi*radius**2
+print(f"Area  of the circle: {area}")
+
